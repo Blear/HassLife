43c12193da4d042071622b40023082cd51d387a1
Blear <723712241@qq.com>
2022-01-13 19:50:15 +0800

    支持集成添加安装配置hasslife账号

--- custom_components/hasslife/__init__.py
+++ custom_components/hasslife/__init__.py
@@ -1,29 +1,46 @@
 from homeassistant.const import (EVENT_HOMEASSISTANT_START,
                                  EVENT_HOMEASSISTANT_STOP, EVENT_STATE_CHANGED)
-
+from homeassistant.config_entries import SOURCE_IMPORT, ConfigEntry
+from homeassistant.const import (
+    CONF_NAME,
+    CONF_PASSWORD,
+)
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
 from .molo_client_config import MOLO_CONFIGS
 from .molo_client_app import MOLO_CLIENT_APP
 from .utils import LOGGER
 import time
 DOMAIN = 'hasslife'
 NOTIFYID = 'hasslifenotifyid'
-VERSION = 101
+VERSION = 103
 is_init = False
 last_start_time = time.time()
 
-def setup(hass, config):
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    # Load config mode from configuration.yaml.
+    hass.data.setdefault(DOMAIN, {})
+
+    if DOMAIN in config:
+        hass.async_create_task(
+                hass.config_entries.flow.async_init(
+                    DOMAIN, context={"source": SOURCE_IMPORT}, data=config[DOMAIN]
+                )
+            )
+    return True
+    
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up molobot component."""
     LOGGER.info("Begin setup hasslife!")
+    hass.data.setdefault(DOMAIN, {})
 
     # Load config mode from configuration.yaml.
-    cfg = config[DOMAIN]
+    cfg = dict(entry.data)
     cfg.update({"__version__": VERSION})
-
     if 'mode' in cfg:
         MOLO_CONFIGS.load(cfg['mode'])
     else:
         MOLO_CONFIGS.load('release')
-
 
     MOLO_CONFIGS.get_config_object()["hassconfig"] = cfg
 
@@ -36,12 +53,10 @@ def setup(hass, config):
     async def start_molobot(event):
         """Start Molobot while starting ha."""
         LOGGER.debug("hasslife started!")
-        hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, stop_molobot)
-        last_start_time = time.time()
-
-    async def hass_started(event):
         global is_init
         is_init = True
+        hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, stop_molobot)
+        last_start_time = time.time()
 
     async def on_state_changed(event):
         """Disable the dismiss button."""
@@ -68,8 +83,13 @@ def setup(hass, config):
     if not cfg.get("disablenotify", False):
         hass.components.persistent_notification.async_create(
             "Welcome to hasslife!", "hasslife Infomation", "hasslife_notify")
-    hass.bus.async_listen_once('homeassistant_started', hass_started)
     hass.bus.async_listen_once(EVENT_HOMEASSISTANT_START, start_molobot)
     hass.bus.async_listen(EVENT_STATE_CHANGED, on_state_changed)
 
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    from .molo_bot_main import stop_aligenie
+    await stop_aligenie()
     return True
---
+++ custom_components/hasslife/config_flow.py
@@ -0,0 +1,50 @@
+import voluptuous as vol
+from typing import Any, Dict, Optional
+from homeassistant import config_entries, core, exceptions
+from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
+from homeassistant.data_entry_flow import FlowResult
+import json       
+from . import DOMAIN
+import aiohttp
+from .utils import LOGGER
+DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_USERNAME): str,
+        vol.Required(CONF_PASSWORD): str,
+    }
+)
+
+class HassLifeConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow"""
+    VERSION = 1
+    async def async_step_user(self, user_input= None) -> FlowResult:
+        """Handle the initial step."""
+        errors: dict[str, str] = {}
+        if user_input is not None:
+            unique_id = f"{user_input[CONF_USERNAME]}"
+            await self.async_set_unique_id(unique_id)
+            self._abort_if_unique_id_configured()
+            info = None
+            payload  = {'username': user_input[CONF_USERNAME], 'password': user_input[CONF_PASSWORD]}
+            async with aiohttp.ClientSession() as session:
+                async with session.post('https://hass.blear.cn/callback.php',data=payload) as response:
+                    if response.status == 200:
+                        data = await response.text()
+                        LOGGER.info("login info: %s", data)
+                        jsondata=json.loads(data)
+                        if jsondata['code'] == 'ok':
+                            info = {'title':user_input[CONF_USERNAME]}
+                        else:
+                            errors["base"] = 'login_error'
+                    else:
+                        errors["base"] = 'server_error'
+            if info:
+                return self.async_create_entry(title=info["title"], data=user_input)
+
+        return self.async_show_form(
+            step_id="user", data_schema=DATA_SCHEMA, errors=errors
+        )
+
+    async def async_step_import(self, user_input: dict[str, Any]) -> FlowResult:
+        """Handle import."""
+        return await self.async_step_user(user_input)
