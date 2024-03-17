fc43a05b5524ba804742272d48e44efaa47ee384
Blear <723712241@qq.com>
2024-03-18 01:57:18 +0800

    remove asyncore

--- custom_components/hasslife/__init__.py
+++ custom_components/hasslife/__init__.py
@@ -8,14 +8,12 @@ from homeassistant.const import (
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
 from .molo_client_config import MOLO_CONFIGS
-from .molo_client_app import MOLO_CLIENT_APP
+from .client import TcpClient
 from .utils import LOGGER
-import time
+
 DOMAIN = 'hasslife'
 NOTIFYID = 'hasslifenotifyid'
-VERSION = 1.9
-is_init = False
-last_start_time = time.time()
+VERSION = 2.0
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     # Load config mode from configuration.yaml.
@@ -33,7 +31,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up molobot component."""
     LOGGER.info("Begin setup hasslife!")
     hass.data.setdefault(DOMAIN, {})
-
     # Load config mode from configuration.yaml.
     cfg = dict(entry.data)
     cfg.update({"version": VERSION})
@@ -41,55 +38,19 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         MOLO_CONFIGS.load(cfg['mode'])
     else:
         MOLO_CONFIGS.load('release')
-
     MOLO_CONFIGS.get_config_object()["hassconfig"] = cfg
-
-    async def stop_molobot(event):
-        """Stop Molobot while closing ha."""
-        LOGGER.info("Begin stop hasslife!")
-        from .molo_bot_main import stop_aligenie
-        stop_aligenie()
-
-    async def start_molobot(event):
-        """Start Molobot while starting ha."""
-        LOGGER.debug("hasslife started!")
-        global is_init
-        is_init = True
-        hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, stop_molobot)
-        last_start_time = time.time()
-
-    async def on_state_changed(event):
-        """Disable the dismiss button."""
-        global is_init
-        global last_start_time
-
-        if MOLO_CLIENT_APP.molo_client:
-            if is_init :
-                MOLO_CLIENT_APP.molo_client.sync_device(True, 2)
-                is_init = False
-            elif last_start_time and (time.time() - last_start_time > 30):
-                last_start_time = None
-                MOLO_CLIENT_APP.molo_client.sync_device(True, 2)
-            elif not is_init or not last_start_time:
-                new_state = event.data.get("new_state")
-                if not new_state:
-                    return
-                MOLO_CLIENT_APP.molo_client.sync_device_state(new_state)
-
-
-    from .molo_bot_main import run_aligenie
-    run_aligenie(hass)
-
-    if not cfg.get("disablenotify", False):
-        hass.components.persistent_notification.async_create(
-            "Welcome to hasslife!", "hasslife Infomation", "hasslife_notify")
-    hass.bus.async_listen_once(EVENT_HOMEASSISTANT_START, start_molobot)
-    hass.bus.async_listen(EVENT_STATE_CHANGED, on_state_changed)
-
+    client = TcpClient(MOLO_CONFIGS.get_config_object()['server']['host'],
+        int(MOLO_CONFIGS.get_config_object()['server']['port']),hass)
+    client.run()
+    hass.data[DOMAIN][entry.entry_id] = {
+        "client": client,
+    }
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    from .molo_bot_main import stop_aligenie
-    await stop_aligenie()
+    data = hass.data[DOMAIN].get(entry.entry_id)
+    if data is not None:
+        data["client"].stop()
+        hass.data[DOMAIN].pop(entry.entry_id)
     return True
---
+++ custom_components/hasslife/client.py
@@ -0,0 +1,304 @@
+import asyncio
+import time
+import struct
+import json
+import hashlib
+import traceback
+import threading
+from homeassistant.const import (__short_version__,EVENT_STATE_CHANGED)
+
+from .const import (BUFFER_SIZE, CLIENT_VERSION, CONFIG_FILE_NAME)
+from .molo_client_config import MOLO_CONFIGS
+from .utils import LOGGER, dns_open, get_rand_char, save_local_seed
+from homeassistant.helpers.json import JSONEncoder
+
+class TcpClient:
+    white_domains = ['button','binary_sensor','sensor','light','cover','switch','vacuum','water_heater','humidifier','fan','media_player','script','climate','input_boolean','automation','group','lock']
+    protocol_func_bind_map = {}
+    is_exited = False
+
+    def __init__(self, host, port,hass):
+        self.host = host
+        self.port = port
+        self.hass = hass
+        self.reader = None
+        self.writer = None
+        self.is_connected = False
+        self.is_init=True
+
+        self._last_report_device = 0
+        self._login_info={}
+        self.init_func_bind_map()
+        self.entity_ids = []
+
+        # 设置心跳间隔（例如：5秒）
+        self.heartbeat_interval = 5
+        # 设置心跳超时时间（例如：60秒）
+        self.heartbeat_timeout = 60
+        self.heartbeat_timer = time.time()
+        self.last_start_time=None
+
+    async def connect(self):
+        self.reader, self.writer = await asyncio.open_connection(self.host, self.port)
+        self.is_connected = True
+        LOGGER.info("Connected to:%s:%d", self.host,self.port)
+        
+
+    async def send_message(self, message):
+        if self.is_connected:
+            # 发送消息前确保连接是打开的
+            message_body = json.dumps(message).encode('utf-8')
+            # 计算消息体的长度  
+            message_length = len(message_body)
+            # 构造首部数据  
+            header_data = struct.pack('<I', message_length)
+            header_data=header_data.ljust(32, b'\x00')
+            #header_data=message_length.to_bytes(32, byteorder='little')
+            self.writer.write(header_data + message_body)
+            await self.writer.drain()
+            LOGGER.info("send :%s", message)
+        else:
+            LOGGER.info("Not connected. Cannot send message.")
+
+    async def receive_message(self):
+        if self.is_connected:
+            while True:
+                header_data = await self.reader.read(32)  
+                if not header_data:  
+                    return None  # 如果没有数据，返回None
+                # 解析首部的unsigned int数据  
+                message_length = int.from_bytes(header_data, byteorder='little')
+                # 读取包体数据  
+                data = await self.reader.read(message_length)
+                #data = await self.reader.read(1024)
+                if data:
+                    LOGGER.info("Received:%s",data.decode())
+                    try:
+                        json_buff = data.decode('utf-8')
+                        body_jdata = json.loads(json_buff)
+                    except (json.JSONDecodeError, UnicodeDecodeError) as exc:
+                        LOGGER.error("recv body error %s",
+                         self.tmp_buffer[:self.body_len])
+                        logging.exception(exc)
+                        return
+                    await self.process_json_pack(body_jdata)
+                else:
+                    current_time = asyncio.get_running_loop().time()
+                    if current_time - self.heartbeat_timer > self.heartbeat_timeout:
+                        LOGGER.info("Heartbeat timed out!")
+                        return
+                    LOGGER.error("Connection closed by server.")
+                    break
+                # 如果收到响应，重置未响应计数器
+                self.heartbeat_timer = asyncio.get_running_loop().time()
+        else:
+            LOGGER.error("Not connected. Cannot receive message.")
+
+    async def close_connection(self):
+        if self.writer is not None:
+            self.writer.close()
+            await self.writer.wait_closed()
+            self.writer = None
+            self.reader = None
+            self.is_connected=False
+            LOGGER.info("Connection closed")
+
+    def run(self):
+        self.last_start_time=time.time()
+        self.hass.bus.async_listen(EVENT_STATE_CHANGED, self.on_state_changed)
+        client_thread = threading.Thread(target=self.main)  
+        client_thread.start()  
+
+    def main(self):
+        # 运行事件循环
+        asyncio.run(self.loop())
+
+    async def loop(self):
+        while not self.is_exited:
+            try:
+                await self.connect()
+                heartbeat_task = asyncio.create_task(self.heartbeat())
+                listen_task = asyncio.create_task(self.receive_message())
+                await listen_task
+            except Exception as e: 
+                LOGGER.error("Connection failed:%s",e)
+                heartbeat_task.cancel()
+                try:
+                    await heartbeat_task
+                except asyncio.CancelledError:
+                    pass
+                await self.close_connection()
+                await asyncio.sleep(5)
+        heartbeat_task.cancel()
+        try:
+            await heartbeat_task
+        except asyncio.CancelledError:
+            pass
+        await self.close_connection()
+
+    def stop(self):
+        self.is_exited=True
+
+    async def heartbeat(self):
+        """发送心跳消息的协程，interval为心跳间隔时间（秒）。"""
+        if self.writer is not None:
+            while True:
+                await self.send_message({"Type":"Ping"})
+                LOGGER.info("Heartbeat send")
+                await asyncio.sleep(self.heartbeat_interval)
+        else:
+            LOGGER.error("Not connected. Cannot send heartbeat.")
+
+    async def sync_device(self, force=False, interval=180):
+        now = time.time()
+        if (not force) and (now - self._last_report_device < interval):
+            return None
+        self._last_report_device = now
+        self._login_info = self.get_login_info()
+        if not self._login_info:
+            return None
+
+        devicelist = self.hass.states.async_all()
+        usefull_entity = []
+        for sinfo in devicelist:
+            dinfo = sinfo.as_dict()
+            entity_id = dinfo['entity_id']
+            domain = self._get_domain(entity_id)
+
+            if domain in self.white_domains:
+                usefull_entity.append(dinfo)
+        
+        jlist = json.dumps(
+                usefull_entity, sort_keys=True, cls=JSONEncoder)
+        if not jlist:
+            return None
+        body = {
+            'Type': 'SyncDevice',
+            'Payload': {
+                'Username': self._login_info['username'],
+                'Password': self._login_info['password'],
+                'Version': self._login_info['version'],
+                'List': jlist
+            }
+        }
+        await self.send_message(body)
+
+    async def sync_device_state(self, state):
+        if not state:
+            return None
+        if not self._login_info:
+            return None
+        entity_id = state.entity_id
+        if entity_id not in  self.entity_ids:
+            return None
+        State = json.dumps(
+            state, sort_keys=True, cls=JSONEncoder)
+
+        body = {
+            'Type': 'SyncState',
+            'Payload': {
+                'Username': self._login_info['username'],
+                'Password': self._login_info['password'],
+                'Version': self._login_info['version'],
+                'State': State
+            }
+        }
+        await self.send_message(body)
+
+    async def on_sync_device(self, jdata):
+        LOGGER.info("sync devices:%s", jdata)
+        await self.sync_device(True)
+
+    async def on_device_control(self, jdata):
+        LOGGER.info("receive device state:%s", jdata)
+        jpayload = jdata['Payload']
+        rows=jpayload['Rows']
+        for row in rows:
+            data = row.get("data")
+            exc = {}
+            try:
+                domain = row.get("domain")
+                service = row.get("service")
+                exc = self.hass.services.call(domain, service, data, blocking=True)
+            except Exception as e:
+                exc = traceback.format_exc()
+
+
+    async def on_auth(self, jdata):
+        LOGGER.info("receive entitys:%s", jdata)
+        body = {
+            'Type': 'Auth',
+            'Payload': {
+                'Username': self._login_info['username'],
+                'Password': self._login_info['password'],
+                'Version': self._login_info['version'],
+            }
+        }
+        await self.send_message(body)
+
+    async def on_update_entitys(self, jdata):
+        LOGGER.info("receive entitys:%s", jdata)
+        jpayload = jdata['Payload']
+        try:
+            self.entity_ids = jpayload.get("entity_ids")
+        except Exception as e:
+            exc = traceback.format_exc()
+
+
+    async def on_error(self, jdata):
+        LOGGER.info("error:%s", jdata)
+        jpayload = jdata['Payload']
+        self.is_exited=True
+        await self.close_connection()
+        try:
+            msg = jpayload.get("msg")
+            LOGGER.error("error:%s", msg)
+        except Exception as e:
+            exc = traceback.format_exc()
+
+
+    def get_login_info(self):
+        if self._login_info:
+            return self._login_info
+        hassconfig = MOLO_CONFIGS.get_config_object().get("hassconfig", {})
+        username = hassconfig.get("username", "")
+        password = hassconfig.get("password", "")
+        version = hassconfig.get("version", 1.0)
+        password = hashlib.sha1(password.encode('utf-8')).hexdigest()
+        self._login_info={'username':username,'password':password,'version':version}
+        return self._login_info
+
+    def _get_domain(self, entity_id):
+        return entity_id.split(".")[0]
+
+
+    async def process_json_pack(self, jdata):
+        """Handle received json packet."""
+        LOGGER.debug("process_json_pack %s", str(jdata))
+        if jdata['Type'] in self.protocol_func_bind_map:
+            await self.protocol_func_bind_map[jdata['Type']](jdata)
+
+    def init_func_bind_map(self):
+        """Initialize protocol function bind map."""
+        self.protocol_func_bind_map = {
+            "DeviceControl": self.on_device_control,
+            "UpdateEntitys": self.on_update_entitys,
+            "Auth": self.on_auth,
+            "Error": self.on_error,
+            "SyncDevice": self.on_sync_device
+        }
+
+    async def on_state_changed(self,event):
+        if self.is_init :
+            await self.sync_device(True, 2)
+            self.is_init=False
+        elif self.last_start_time and (time.time() - self.last_start_time > 30):
+            self.last_start_time = None
+            await self.sync_device(True, 2)
+        elif not self.is_init or not last_start_time:
+            new_state = event.data.get("new_state")
+            if not new_state:
+                return
+            await self.sync_device_state(new_state)
+
+
