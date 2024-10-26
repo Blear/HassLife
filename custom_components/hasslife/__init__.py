42d5d3988312a6eee46928ef9ad7ad26d9c572da
Blear <723712241@qq.com>
2024-10-26 14:41:44 +0800

    Update __init__.py

--- custom_components/hasslife/__init__.py
+++ custom_components/hasslife/__init__.py
@@ -13,7 +13,7 @@ from .utils import LOGGER
 
 DOMAIN = 'hasslife'
 NOTIFYID = 'hasslifenotifyid'
-VERSION = 3.2
+VERSION = 3.3
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     # Load config mode from configuration.yaml.
